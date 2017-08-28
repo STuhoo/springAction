7b52ad174b2c18241e58ffacdcd94cb93c6834a8
CYZ <2541208684@qq.com>
2017-08-28 10:58:08 +0800

    chapter3 with all spec
    add acutator feature at this version.

---
+++ springSample/chapter3/src/main/java/com/smart/Application.java
@@ -0,0 +1,42 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+
+@RestController
+//@EnableAutoConfiguration
+@SpringBootApplication
+@EnableTransactionManagement //启动事务管理,自动装配了事务支持
+public class Application extends SpringBootServletInitializer{
+
+/*    @RequestMapping("/") //对应关系：URL<-->Function
+    public String index(){
+        return "欢迎光临小春论坛！";
+    }
+*/
+    @Bean  //自定义事务管理器，
+    public PlatformTransactionManager txManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    //@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        //return super.configure(builder); //自动生成代码
+        return application.sources(Application.class);//看不懂！！！！
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
